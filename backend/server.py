ac5e73ef2b16c25665fb97f547fa86efb9cbc022
Johann Bauer <jb592@uni-rostock.de>
2018-10-24 19:03:13 +0200

    Fix issue with parameter order

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-import importlib
+import importlib, inspect
 import logging
 import sys
 import traceback
@@ -39,7 +39,7 @@ async def start_attack(sid, name, params):
     args = [db]
     for a in attacks:
         if a.__name__ == name:
-            for k, t in a.__init__.__annotations__.items():
+            for k in inspect.getargspec(a.__init__)[0][2:]:
                 try:
                     args.append(params[k])
                 except KeyError:
