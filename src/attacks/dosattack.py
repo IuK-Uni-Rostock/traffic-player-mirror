30f44a47c81653ac4970a863ddaa95f7a04b2b3d
mj244 <mj244@uni-rostock.de>
2019-01-10 22:04:16 +0100

    Change DoS duration to SliderType

--- src/attacks/dosattack.py
+++ src/attacks/dosattack.py
@@ -12,22 +12,21 @@ class DoSAttack(Attack):
         "icon": "mdi-run-fast"
     }
 
-    # noinspection PyUnresolvedReferences,PyTypeChecker
     def __init__(self, database,
                  target_players: LogPlayerType(),
                  workload: SliderType(1, 100),
-                 duration: TimeSliderType(1, 60*60*24),  # 1s-24h
+                 duration: SliderType(1, 600), # 1s to 10min
                  telegram_types: MultipleChoiceType(('A_GroupValue_Write')),
                  target_addresses: TextfieldType(),
                  seed: TextfieldType(default=314159265359)):
         super().__init__(database, seed, target_players)
-        self.__workload = workload
+        self.__workload = workload / 100
         self.__duration = duration
         self.__telegram_types = telegram_types
         self.__target_addresses = target_addresses
 
     def prepare(self):
         g = Generator(self._seed)
-        telegrams = g.generate(self.__telegram_types, self.__duration, self.__workload / 100)
+        telegrams = g.generate(self.__telegram_types, self.__duration, self.__workload)
         if not telegrams is None:
             self._manipulator.telegrams = telegrams
--- src/attacks/replayattack.py
+++ src/attacks/replayattack.py
@@ -22,11 +22,11 @@ class ReplayAttack(Attack):
         self.__replay_speed = replay_speed
         self.__start_time = start_time
         self.__end_time = end_time
-        self.__selection_rate = selection_rate
+        self.__selection_rate = selection_rate / 100
 
     def prepare(self):
         telegrams = self._db.get_telegrams(self.__start_time, self.__end_time)
         if not telegrams is None:
             self._manipulator.telegrams = telegrams
             self._manipulator.adjust_time_between_telegrams(self.__replay_speed)
-            self._manipulator.filter_percentage(self.__selection_rate / 100)
+            self._manipulator.filter_percentage(self.__selection_rate)
