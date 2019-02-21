24f944e2c5ddb9f70a8e1c0a967f398efeda17bb
mj244 <mj244@uni-rostock.de>
2019-02-21 17:07:43 +0100

    Add support for simulating multiple walking persons

--- src/walkgenerator.py
+++ src/walkgenerator.py
@@ -14,7 +14,7 @@ class MotionSensor:
 
     def activate(self, current_time):
         if current_time - self.last_activation_time >= self.reactivation_time:
-            print('{0} activated at time {1}'.format(self.__str__(), current_time))
+            #print('{0} activated at time {1}'.format(self.__str__(), current_time))
             self.last_activation_time = current_time
             return self.__create_telegram(current_time, 1, self.source_address, self.destination_address)
         return None
@@ -41,6 +41,13 @@ class MotionSensor:
     def __str__(self):
         return '[{0} -> {1} ; {2}]'.format(self.source_address, self.destination_address, self.reactivation_time)
 
+class WalkGeneratorConfig:
+    def __init__(self, walking_speed, jitter, start, destination, custom_path=None):
+        self.walking_speed = walking_speed
+        self.jitter = jitter
+        self.start = start
+        self.destination = destination
+        self.custom_path = custom_path
 
 class WalkGenerator:
     def __init__(self, model, seed):
@@ -49,8 +56,34 @@ class WalkGenerator:
         self.G = model
         self.__telegram_sequence = []
 
+    def generate_multiple(self, configs):
+        sensors = []
+        for _, node in self.G.nodes(data=True):
+            if 'sensor' in node:
+                sensors.append(node['sensor'])
+
+        walks = []
+        for person in configs:
+            walks.append(self.generate(person.walking_speed, person.jitter, person.start, person.destination, person.custom_path))
+
+        telegrams = [walk for walk in walks][0]
+        telegrams.sort(key=lambda t: t.timestamp)
+
+        to_remove = []
+        for idx, telegram in enumerate(telegrams):
+            sensor = next((s for s in sensors if s.source_address == telegram.source_addr and s.destination_address == telegram.destination_addr), None)
+            for tdx, t in enumerate(telegrams):
+                if tdx > idx and t.timestamp <= telegram.timestamp + sensor.reactivation_time and t.source_addr == telegram.source_addr and t.destination_addr == telegram.destination_addr:
+                    to_remove.append(tdx)
+
+        for index in sorted(set(to_remove), reverse=True):
+            del telegrams[index]
+
+        return telegrams
+
     def generate(self, walking_speed, jitter, start, destination, custom_path=None):
         # path = [1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,18,3,4,5,6,7,8,9,10,13,14,15,16,17,18,3,2,1]
+        self.__reset()
         if custom_path is None:
             path = weighted.dijkstra_path(self.G, start, destination)
         else:
@@ -67,8 +100,13 @@ class WalkGenerator:
             last_node = node
         return self.__telegram_sequence
 
+    def __reset(self):
+        for _, node in self.G.nodes(data=True):
+            if 'sensor' in node:
+                node['sensor'].last_activation_time = -999999
+
     def __move(self, node, time):
-        print('Moving to node {0}'.format(node))
+        #print('Moving to node {0}'.format(node))
         node = self.G.node[node]
         if 'sensor' in node:
             result = node['sensor'].activate(time)
