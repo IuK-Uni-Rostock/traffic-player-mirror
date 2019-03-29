e6973f5a1d2d395f753fa63d583384a7565c15be
mj244 <mj244@uni-rostock.de>
2019-03-29 10:40:38 +0100

    Add user-friendly targets to DoS attacks

--- src/attacks/dosattack.py
+++ src/attacks/dosattack.py
@@ -1,6 +1,6 @@
 from src.attacks.attack_parameters import (LogPlayerType, MultipleChoiceType,
                                            SliderType, TextfieldType,
-                                           TimeSliderType)
+                                           TimeSliderType, SingleChoiceType)
 
 from src.generator import Generator
 from src.attacks.attack import Attack
@@ -17,16 +17,16 @@ class DoSAttack(Attack):
                  workload: SliderType(1, 100),
                  duration: SliderType(1, 600), # 1s to 10min
                  telegram_types: MultipleChoiceType(('A_GroupValue_Write')),
-                 target_addresses: TextfieldType(),
+                 target: SingleChoiceType(('Random', 'LED Strips')),
                  seed: TextfieldType(default=314159265359)):
         super().__init__(database, seed, target_players)
         self.__workload = workload / 100
         self.__duration = duration
         self.__telegram_types = telegram_types
-        self.__target_addresses = target_addresses
+        self.__target = target
 
     def prepare(self):
         g = Generator(self._seed)
-        telegrams = g.generate(self.__telegram_types, self.__duration, self.__workload)
+        telegrams = g.generate(self.__telegram_types, self.__target, self.__duration, self.__workload)
         if not telegrams is None:
             self._manipulator.telegrams = telegrams
--- src/generator.py
+++ src/generator.py
@@ -9,28 +9,37 @@ class Generator:
         self._rng = Random()
         self._rng.seed(seed)
 
-    def generate(self, types, duration, workload):
+    def generate(self, types, target, duration, workload):
         """Generates a pseudo-random sequence of telegrams using the given telegram types."""
         assert duration > 0, "duration must be > 0"
         assert workload > 0 and workload <= 1, "workload must be > 0 and <= 100"
         assert len(types) > 0, "no types available"
+        assert target, "no target available"
 
         # assume 60 telegrams per second == 100% workload
         amount = int(60 * workload * duration)
 
         result = []
         for i in range(1, amount):
-            result.append(self.__create_telegram(self._rng.choice(types)))
+            result.append(self.__create_telegram(self._rng.choice(types), target))
         return result
 
-    def __create_telegram(self, telegram_type):
+    def __create_telegram(self, telegram_type, target):
         t = Telegram()
         is_group_address = True if "Group" in telegram_type else False
         t.source_addr = "{0}.{1}.{2}".format(self._rng.randint(1, 15), self._rng.randint(1, 15), self._rng.randint(1, 255))
-        if is_group_address:
-            t.destination_addr = "{0}/{1}/{2}".format(self._rng.randint(1, 31), self._rng.randint(1, 7), self._rng.randint(1, 255)) # 0/0/0 not allowed
-        else:
-            t.destination_addr = "{0}.{1}.{2}".format(self._rng.randint(1, 15), self._rng.randint(1, 15), self._rng.randint(1, 255))
+        if target == 'Random':
+            if is_group_address:
+                t.destination_addr = "{0}/{1}/{2}".format(self._rng.randint(1, 31), self._rng.randint(1, 7), self._rng.randint(1, 255)) # 0/0/0 not allowed
+            else:
+                t.destination_addr = "{0}.{1}.{2}".format(self._rng.randint(1, 15), self._rng.randint(1, 15), self._rng.randint(1, 255))
+        elif target == 'LED Strips':
+            if is_group_address:
+                strip_group_addresses = ['1/1/1', '1/1/3']
+                t.destination_addr = self._rng.choice(strip_group_addresses)
+        #    else: TOOD: figure out LED strip physical addresses
+        #        strip_addresses = ['']
+        #        t.destination_addr = self._rng.choice(strip_addresses)
         t.system_broadcast = 1 # constant
         t.repeat = 1 # constant?
         t.priority = self._rng.randint(0,3)
