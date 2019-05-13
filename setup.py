dd33b325c6503568971bf5d3d917d302631ad3c4
mj244 <mj244@uni-rostock.de>
2019-05-13 20:43:46 +0200

    Add dependency versions for repeatability

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.0.1',  # Required
     packages=find_packages(exclude=['frontend']),  # Required
     install_requires=[
-        'aiohttp',
-        'aiohttp-index',
-        'python-socketio',
-        'mysql-connector',
-        'pika',
-        'networkx',
+        'aiohttp>=3.0,<4',
+        'aiohttp-index>=0.1',
+        'python-socketio>=4.0,<5',
+        'mysql-connector>=2.0,<3',
+        'pika>=1.0,<2',
+        'networkx>=2.0,<3',
         'knxmap @ git+https://github.com/takeshixx/knxmap.git'
     ],
 )
